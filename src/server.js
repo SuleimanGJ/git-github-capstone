ce8a22461f399234cae10bc4916eb78d147e2734
SuleimanByte <suleimanosman1023@gmail.com>
2026-08-16 17:11:28 +0300

    refactor: separate express app from server startup

--- src/server.js
+++ src/server.js
@@ -1,16 +1,5 @@
-import express from "express";
 import { PORT } from "./config/index.js";
-import { connectDB } from "./db/db.js";
-import { todoRouter } from "./routes/index.js";
-const app = express();
-
-app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.send("App is working...")
-});
-
-app.use("api/todo", todoRouter);
+import app from "./app.js";
 
 connectDB.then(() => {
     app.listen(PORT, () => {
