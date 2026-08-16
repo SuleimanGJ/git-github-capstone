1a5ec9afd22d0c530ee29e7ed4c50ca7e48b7864
SuleimanByte <suleimanosman1023@gmail.com>
2026-08-16 12:27:35 +0300

    feat: add todos creation endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,11 +12,24 @@ router.get("/", async (req, res) => {
 
 router.get("/id", async (req, res) => {
     const todoId = req.params.id;
-    const todos = await TodoModel.findOne({_id: todoId});
+    const todos = await TodoModel.findOne({ _id: todoId });
     return res.json({
         data: todos,
         message: "Todos fetched successfully"
     });
 });
+
+
+router.put("/", async (req, res) => {
+    const { title, description, completed } = req.body;
+
+    const todos = await TodoModel({ title, description, completed })
+    await todos.save();
+
+    return res.json({
+        data: todos,
+        message: "Todo created successfully"
+    });
+})
 
 export { router };
